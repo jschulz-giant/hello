bab966d5ca92fa03a9715343829c99e9094e7d6a
jeff schulz <jschulz@giant-interactive.com>
2017-09-29 08:41:57 -0700

    changes

--- src/main/java/com/giant/hello/Goodbye.java
+++ src/main/java/com/giant/hello/Goodbye.java
@@ -1,5 +1,9 @@
 
 public class Goodbye
 {
+    private final String region;
 
+    public Goodbye( final String region ) {
+        this.region = region;
+    }
 }
---
+++ src/main/kotlin/com/giant/hello/greetings.kt
@@ -0,0 +1,7 @@
+
+class Greetings
+{
+    init {
+        
+    }
+}
